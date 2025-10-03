8b48815a23b6ac87be386987021b39537ab7bcca
putrareno <ptraarenoo@gmail.com>
2025-10-03 17:56:43 +0700

    Belajar C++

---
+++ HelloReno.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+        cout<<"Hello  Reno"<<endl;//Menampilkan Output
+        system("pause"); //Memberi jeda sebelum program ditutup 
+    return 0;
+}
---
+++ inputoutput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string nama;
+    int usia;
+        cout<<"Masukkan Nama Anda: ";
+        cin>>nama;
+        cout<<"Masukkan Usia Anda: ";
+        cin>>usia;
+
+        cout<<"Nama Anda: "<<nama<<endl;
+        cout<<"Usia Anda: "<<usia<<endl;
+    return 0;
+}
