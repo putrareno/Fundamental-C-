28cd2ea94ef2c319f3de0b6f578a148baf19f6cb
putrareno <ptraarenoo@gmail.com>
2025-10-03 19:27:59 +0700

    Tambah tipe data hobi di inputoutput.cpp

--- inputoutput.cpp
+++ inputoutput.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 int main(){
 
-    string nama;
+    string nama,hobi;
     int usia;
         cout<<"Masukkan Nama Anda: ";
         cin>>nama;
+        cout<<"Masukkan Hobi Anda: ";
+        cin>>hobi;
         cout<<"Masukkan Usia Anda: ";
         cin>>usia;
 
         cout<<"Nama Anda: "<<nama<<endl;
+        cout<<"Hobi Anda: "<<hobi<<endl;
         cout<<"Usia Anda: "<<usia<<endl;
+
+        system("pause");
     return 0;
 }
